16d2a0129864fdd3095e64dae86ee5212dfc388d
RobTurner-coder <rt76398@gmail.com>
2025-06-05 12:12:02 +0100

    Add attacks and basic functionality

--- classes.py
+++ classes.py
@@ -1,6 +1,62 @@
+import random
 class Player:
 # initialises player class
 	def __init__(self):
-		self.name = input("What is your name >> ")
+		self.name = input("What is your name >>  ")
 		self.health = 100
+		self.attack = 10
+		self.ice = 20
+		self.healing = 30
 
+	def get_attack(self, target):#gets user input for attack
+		user_input = input("What would you like to do? 1 = ATTACK | 2 = ICE | 3 = HEAL >> ")
+		if user_input == "1":#basic attack
+			self.basic_attack(target)
+			return self.attack
+		elif user_input == "2":#ice attack
+			self.ice_attack(target)
+			return self.ice
+		elif user_input == "3":
+			self.heal()
+
+	def basic_attack(self, target):
+			target.health -= self.attack
+			print(f"{self.name} attacked {target.name} with a basic attack for {self.attack} damage")
+			
+	
+	def ice_attack(self, target):
+		target.health -= self.ice
+		print(f"{self.name} attacked {target.name} with an ice attack for {self.ice} damage")
+
+	def heal(self):
+		heal_amount = self.healing
+		self.health = min(self.health + heal_amount, 100)
+		print(f"You healed yourself for {self.healing} hit points! You now have {self.health} hit points")
+
+	
+
+class Computer:
+	def __init__(self):
+		self.name = "Gilgamesh"
+		self.health = 100
+		self.attack = 10
+		self.ice = 20
+		self.mega_flare = 50
+
+	def computer_turn(self, target):
+		attack_list = [self.basic_attack, self.ice_attack, self.mega_flare_attack]
+		random_attack = random.choice(attack_list)
+		random_attack(target)
+		
+
+	def basic_attack(self, target):
+		target.health -= self.attack
+		print(f"{self.name} attacks you with a basic attack doing {self.attack} damage!")
+	
+	def ice_attack(self, target):
+		target.health -= self.ice
+		print(f"{self.name} attacks you with ice for {self.ice} damage")
+
+	def mega_flare_attack(self, target):
+		target.health -= self.mega_flare
+		print(f"{self.name} attacks you with MEGA FLARE for {self.mega_flare} damage")
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from classes import *
 def main():
+	player1 = Player()
+	player2 = Computer()
+	print(f"Hello {player1.name}")
+	print(f"My name is {player2.name}. I am going to destroy you!")
 	start = True
 	while start == True:
-		player1 = Player()
-		print(f"Hello {player1.name}")
-		start = False
+		
+		player1.get_attack(player2)
+		print(f"{player2.name} has {player2.health} left")		
+		player2.computer_turn(player1)
+		print(f"Your health is {player1.health}")
+		if player2.health <= 0:
+			print(f"{player2.name} is defeated!")
+			print("You win!")
+			start = False
+		elif player1.health <= 0:
+			print(f"{player1.name} is defeaed!")
+			print("Game over!")
+			start = False
 
 main()
