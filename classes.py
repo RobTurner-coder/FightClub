e1faf20487314a8dd4e7901c9f97ab4cc77dc7fc
RobTurner-coder <rt76398@gmail.com>
2025-06-05 10:20:05 +0100

    Add classes and main file

---
+++ classes.py
@@ -0,0 +1,6 @@
+class Player:
+# initialises player class
+	def __init__(self):
+		self.name = input("What is your name >> ")
+		self.health = 100
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from classes import *
+def main():
+	start = True
+	while start == True:
+		player1 = Player()
+		print(f"Hello {player1.name}")
+		start = False
+
+main()
